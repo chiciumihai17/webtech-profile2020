6372d645c125329495a3f0b7ed576a2fb3c2e293
Ubuntu <chiciumihai17@stud.ase.ro>
2019-11-13 10:10:27 +0000

    modified app.js from Lab06

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const express = require("express");
 const app = express();
 
 app.use('/', express.static('frontend'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.get("/home", (req, res) => {
    res.send("Hello World");
 });
 
-console.log(process.env.IP);
+app.get("/messages", (req, res) => {
+    var messages = [];
+    var message = {
+        name: "Mihai",
+        text: "Hello from the web"
+    };
+    messages.push(message);
+    res.status(300).send(messages);
+});
+
+app.post("/messages", (req, res) => {
+    console.log(req.body);
+    // console.log(req)
+    res.send("Data: " + req.body);
+});
 
 app.listen(3000, () => {
     console.log("Server has started!");
